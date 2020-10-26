da645964bb8a01a2528bbbef42117ab10fff9b42
Shahlo <jaborova.shahlo@mail.ru>
2020-10-26 22:57:17 +0500

    edit function

--- pkg/search/search.go
+++ pkg/search/search.go
@@ -1,6 +1,8 @@
 package search
 
 import (
+	"bufio"
+	"os"
 	"context"
 	"io/ioutil"
 	"log"
@@ -78,6 +80,57 @@ func FindAllMatchTextInFile(phrase, fileName string) (res []Result) {
 	}
 
 	return res
+}
+
+//Any
+func Any(ctx context.Context, phrase string, files []string) <-chan Result {
+	if files == nil {
+		return nil
+	}
+	part := len(files)
+	ch := make(chan Result, part)
+	//defer close(ch)
+	//ctxx, cansel := context.WithCancel(ctx)
+	for i := 0; i < part; i++ {
+		go func(ctx1 context.Context, fileOpen string, phrase string, c chan<- Result) {
+			select {
+			case <-ctx1.Done():
+				return
+			default:
+				file, err := os.Open(fileOpen)
+				if err != nil {
+					return
+				}
+				defer func() {
+					if cerr := file.Close(); cerr != nil {
+						log.Print(cerr)
+					}
+				}()
+				reader := bufio.NewReader(file)
+				lineNum := 1
+				for {
+					line, _, err := reader.ReadLine()
+					if err != nil || len(line) == 0 {
+						break
+					}
+					if strings.Contains(string(line), phrase) {
+						result := Result{}
+						colNum := strings.Index(string(line), phrase)
+						result.Phrase = phrase
+						result.ColNum = int64(colNum)
+						result.Line = string(line)
+						result.LineNum = int64(lineNum)
+						c <- result
+						break
+					}
+					lineNum++
+				}
+			}
+		}(ctx, files[i], phrase, ch)
+	}
+	//<-ch
+	//cansel()
+	return ch
 }
 
 // //Any 
